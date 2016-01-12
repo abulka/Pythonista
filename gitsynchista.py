f8642702eb6fe45bca12a0b8950f472462168425
Michael Wagner <miwagner1@icloud.com>
2016-01-11 18:07:05 -0600

    first comment

---
+++ gitsynchista.py
@@ -0,0 +1,98 @@
+import sys
+import os
+
+import log
+import config
+import sync_config
+import sync
+import sync_selector
+import working_copy
+
+reload(log)
+reload(config)
+reload(sync_config)
+reload(sync)
+reload(sync_selector)
+reload(working_copy)
+  
+
+def load_config_file_and_sync(config_filename):
+  
+  global logger
+  
+  config_handler = config.ConfigHandler(sync_config.SyncConfig())
+  tool_sync_config = config_handler.read_config_file(config_filename)
+  
+  sync_tool = sync.SyncTool(tool_sync_config)
+  
+  try:
+    
+    sync_tool.scan()
+    
+  except Exception as e:
+    
+    logger.error("Exception %s during scan" % str(e))
+    return
+  
+  try:
+    
+    sync_tool.sync()
+    
+  except Exception as e:
+    
+    logger.error("Exception %s during sync" % str(e))
+  
+def wakeup_webdav_server(webdav_config):
+  
+    working_copy_support = working_copy.WorkingCopySupport(webdav_config)
+    working_copy_support.wakeup_webdav_server()
+
+def start_gui(check_wakeup):
+  
+  global logger
+  
+  sync_tools = []
+  configs = sync.find_sync_configs()
+  working_copy_configs = filter(lambda config:config.repository.working_copy_wakeup, configs)
+  
+  if len(working_copy_configs) > 0 and check_wakeup:
+    wakeup_webdav_server(working_copy_configs[0].webdav)
+    logger.info("Exiting to await callback from Working Copy...")
+    return 
+    
+  logger.info("Starting GUI...")
+  for config in configs:
+    
+    logger.info("Found configuration '%s'..." % config.repository.name)
+    
+    try:
+      
+      sync_tool = sync.SyncTool(config)
+      #sync_tool.scan()
+      sync_tools.append(sync_tool)
+      
+    except Exception as e:
+      
+      logger.info("Error '%s' while processing configuration '%s'" % ( str(e), config.repository.name) )
+    
+  selector = sync_selector.SyncSelector()
+  
+  selector.select(sync_tools)
+  
+def main():
+  
+  global logger
+  
+  logger = log.open_logging()
+  
+  if len(sys.argv) == 2:  
+    if (sys.argv[1].startswith(working_copy.PARAM_IGNORE_WAKEUP)):
+      start_gui(False)
+    else:
+      load_config_file_and_sync(sys.argv[1])
+  else:
+    start_gui(True)
+  
+if __name__ == '__main__':
+  main()
+  
---
+++ test.py
@@ -0,0 +1,27 @@
+import log
+import config
+import sync_config
+
+reload(log)
+reload(config)
+reload(sync_config)
+
+global logger
+
+logger = log.open_logging()
+
+def test():
+  
+  global logger
+  
+  logger.info("Start test")
+  config_handler = config.ConfigHandler(sync_config.SyncConfig())
+  
+  sample_config = config_handler.read_config_file('etc/gitsynchista_config_sample')
+  
+  sample_config.dump()
+  logger.info("End test")
+  
+if __name__ == '__main__':
+  test()
+
